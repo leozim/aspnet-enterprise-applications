8e2fd35027de13d28a1ed9efa71d5dae5bca6eef
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-09-05 00:11:24 -0300

    regisro e autenticacao usuario

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using NSE.Identidade.API.Models;
 
 namespace NSE.Identidade.API.Controllers
 {
+    [Route("api/identidade")]
     public class AuthController : Controller
     {
         private readonly SignInManager<IdentityUser> _signInManager;
